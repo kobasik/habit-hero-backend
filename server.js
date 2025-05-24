9fc6a5ca6c80a900be51095439075cbe3b5cb2c2
kobasik <kobasik23@gmail.com>
2025-05-24 13:01:40 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(bodyParser.json());
 
 const merchantId = '682c6381dfc9ac0473670ecb';
 const testAuth = 'Basic ' + Buffer.from('Paycom:iw9?YRYgtNC?A2f#3gFC4zYa6fn6#2MHC1Bj').toString('base64');
 
 app.post('/pay', async (req, res) => {
-    const { amount, order_id } = req.body;
+    const { amount, order_id, name, phone } = req.body;
     try {
         const createReceipt = await axios.post('https://test.paycom.uz/api', {
             method: 'receipts.create',
@@ -25,8 +27,11 @@ app.post('/pay', async (req, res) => {
                 'Content-Type': 'application/json'
             }
         });
+
+        console.log(`Создан платёж на ${amount} сум от ${name} (${phone})`);
         res.json(createReceipt.data);
     } catch (error) {
+        console.error('Ошибка при создании чека:', error.response?.data || error.message);
         res.status(500).json({ error: error.toString() });
     }
 });
