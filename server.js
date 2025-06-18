f5391e6764e00afd25eccc65e1a222e77470b47a
kobasik <kobasik23@gmail.com>
2025-06-18 16:39:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 
 dotenv.config();
-const app = express();
 app.use(bodyParser.json());
 
 const PAYME_ID = process.env.PAYME_ID;
